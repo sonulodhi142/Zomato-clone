f35c97f90cf6a4f92b8c423c402dde8623fe4685
Sonu Lodhi <sonulodhis142@gmail.com>
2024-08-21 11:55:09 +0530

    try to adding slider in menu

---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,15 @@
+import React from "react"; 
+import './Slider.css'
+
+
+const Slider = () =>{
+
+    return(
+        <div>
+            <h1>Slider</h1>
+            
+        </div>
+    )
+}
+
+export default Slider;
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './Menu.css'
 import all_product from "../Components/Assets/all_product"
 import { Link } from "react-router-dom";
+import Slider from "../Components/Slider/Slider";
 
 
 const Menu = ( ) => {
@@ -10,18 +11,26 @@ const Menu = ( ) => {
     
     
     return(
-        cart.map((e)=>{
-                return <div className="card cartitems-format cartitems-format-main cart" style={{width : '25rem'}}>
-                <img className="card-img-top" src={e.image} alt="#"/>
-                <div className="card-body">
-                  <h5 className="card-title">{e.name}</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <p className="new-price">${e.new_price} <span className="old-price">${e.old_price}</span></p>
-                  <button className="cartitems-quantity"><Link style={{textDecoration: 'none', color: "black"}} to='/desciption'>Order Now</Link></button>
-                </div>
+        <div>
+          <Slider/>
+          <div className="card-container">
+          {
+            cart.map((e)=>{
+              return <div className="card cartitems-format cartitems-format-main cart" style={{width : '25rem'}}>
+                
+              <img className="card-img-top" src={e.image} alt="#"/>
+              <div className="card-body">
+                <h5 className="card-title">{e.name}</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="new-price">${e.new_price} <span className="old-price">${e.old_price}</span></p>
+                <button className="cartitems-quantity"><Link style={{textDecoration: 'none', color: "black"}} to='/desciption'>Order Now</Link></button>
               </div>
-        }
-    )
+            </div>
+      }
+  )
+          }
+        </div>
+        </div>
  )
 }
 export default Menu;
