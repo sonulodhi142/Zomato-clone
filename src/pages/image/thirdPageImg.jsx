32f1cc240db791a47c013cacd60f3a1cb7fc5729
Sonu Lodhi <sp95759201@gmail.com>
2025-12-22 22:06:56 +0530

    making object of images

--- src/pages/image/thirdPageImg.jsx
+++ src/pages/image/thirdPageImg.jsx
@@ -2,3 +2,52 @@ import img1 from './VegMode.avif'
 import img2 from './healthy.avif'
 import img3 from './foodOnTrain.avif'
 import img4 from './giftCards.avif'
+import img5 from './gourmet.avif'
+import img6 from './mobile.avif'
+import img7 from './offer.avif'
+import img8 from './planAParty.avif'
+
+
+export const page3_data = [
+    {
+        id : 1,
+        name : 'Veg Mode',
+        img : img1
+    },
+    {
+        id : 2,
+        name : 'Healthy',
+        img : img2
+    },
+    {
+        id : 1,
+        name : 'Veg Mode',
+        img : img1
+    },
+    {
+        id : 1,
+        name : 'Veg Mode',
+        img : img1
+    },
+    {
+        id : 1,
+        name : 'Veg Mode',
+        img : img1
+    },
+    {
+        id : 1,
+        name : 'Veg Mode',
+        img : img1
+    },
+    {
+        id : 1,
+        name : 'Veg Mode',
+        img : img1
+    },
+    {
+        id : 1,
+        name : 'Veg Mode',
+        img : img1
+    },
+    
+]
