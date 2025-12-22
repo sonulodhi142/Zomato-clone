07771c2df8d646f85c720dfee0d9081ce0f66567
Sonu Lodhi <sp95759201@gmail.com>
2025-12-22 17:19:06 +0530

    add more img

--- src/pages/image/thirdPageImg.jsx
+++ src/pages/image/thirdPageImg.jsx
@@ -1,3 +1,4 @@
-import vegMode from './VegMode.avif'
-import healthy from './healthy.avif'
-import 
+import img1 from './VegMode.avif'
+import img2 from './healthy.avif'
+import img3 from './foodOnTrain.avif'
+import img4 from './giftCards.avif'
